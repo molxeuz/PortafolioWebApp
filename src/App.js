90df5219ec4aaacb4689a0dd129dd2b16858ce18
molxeuz <mateo.moligon@gmail.com>
2023-05-30 14:57:07 -0500

    parte de proyectos 1/2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <section class="seccion_uno">
+
+        </section>
+
+        <section class="seccion_dos">
+
+        {/* titulo de la seccion */}
+        
+        <div class="container">
+          <div class="text-box">
+            <h1>PROYECTOS</h1>
+            <h1>PROYECTOS</h1>
+          </div>
+        </div>
+
+        {/* Codigo de la seccion de proyectos */}
+
+        <div class="proyectos">
+          <div class="card">
+              <div class="img-card" style={{backgroundImage: 'url(https://www.paginaswebs.com/wp-content/uploads/2017/12/Kevin-R.-Witman-pluto_animation_red_arrow_2s_forever_1469761409.gif)', backgroundSize: 'cover'}}></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card" style={{backgroundImage: 'url(https://th.bing.com/th/id/R.321577d05658685a9c185146734241f1?rik=mbm4Mkr8s26GUg&riu=http%3a%2f%2f3.bp.blogspot.com%2f-blGGYihPR3c%2fVT9Yb9B992I%2fAAAAAAAAkhI%2fDpQZrELDUns%2fs1600%2f25th-gallery-050.jpg&ehk=1Xp2p56a8GRqB1%2bKYeYYe%2bcMdI%2fm6kCqJ5PzLFB%2fwpU%3d&risl=&pid=ImgRaw&r=0)', backgroundSize: 'cover'}}></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          <div class="card">
+              <div class="img-card"></div>
+              <div class="info-card">
+                  <p><strong>Lorem ipsum</strong></p>
+                  <p>Price: 15$</p>
+              </div>
+          </div>
+          
+          
+        </div>
+
+        <div class="close_title"></div>
+
+        </section>
+
+        <section class="seccion_tres">
+          
+        </section>
+
     </div>
   );
+  
 }
 
 export default App;
